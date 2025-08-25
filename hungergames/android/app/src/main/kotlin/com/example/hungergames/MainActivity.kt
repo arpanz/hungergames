33b2da28333d52ae39bca59ab9792e7c2d4744ff
arpanz <arpansinghbsb@gmail.com>
2025-08-25 19:55:26 +0530

    first commit

---
+++ hungergames/android/app/src/main/kotlin/com/example/hungergames/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hungergames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hungergames/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hunger Games',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hunger Games')),
+      body: Center(child: Text('Welcome to the Hunger Games!')),
+    );
+  }
+}
